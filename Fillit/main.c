3a0f4f40d2aa85ba815cbd3d649585951a566d5f
Murch Direct <mdirect@oa-o3.21-school.ru>
2019-11-03 17:52:08 +0300

    a little bit changes in main.c

--- Fillit/main.c
+++ Fillit/main.c
@@ -6,10 +6,11 @@
 /*   By: kroselin <kroselin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 14:22:38 by kroselin          #+#    #+#             */
-/*   Updated: 2019/11/03 16:33:14 by mdirect          ###   ########.fr       */
+/*   Updated: 2019/11/03 17:39:48 by mdirect          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "fillit.h"
 
 int 	main(int ac, char **av)
@@ -21,24 +22,24 @@ int 	main(int ac, char **av)
 	av[1] = "test_1.txt";
 	ac = 2;
 
-	i = 1;
-	if (ac == 2)
-	{
-		fd = open(av[i], O_RDONLY);
-		if (fd < 0)
-			ft_putstr("No file with such name.\n");
-		if (!(tetramins = (uint64_t *)malloc(sizeof(uint64_t) * 26)))
-			return (0);
-		ft_bzero(tetramins, sizeof(tetramins) * 26);
-		if (!(is_valid(fd, &tetramins)))
-			ft_putstr ("It's not a square/tetramine.\n");
-		else
-			ft_putstr ("it's a square and right tetramin.\n");
-	}
-
+	if (ac != 2)
+		return (write(1, "usage: ./fillit input_tetraminos\n", 33));
+	if ((fd = open(av[1], O_RDONLY)) < 0)
+		return (write(1, "No file with such name.\n", 24));
+	if (!(tetramins = (uint64_t *)malloc(sizeof(uint64_t) * 26)))
+		return (write(1, "error: malloc tetraminos\n", 25));
+	ft_bzero(tetramins, sizeof(tetramins) * 26);
+	if (!(is_valid(fd, &tetramins)))
+		return (write(1, "It's not a square/tetramine.\n", 29));
+	else
+		ft_putstr ("it's a square and right tetramin.\n");
+	if (close(fd) == -1)
+		return (write(1, "error: close fd\n", 16));
+	
 	i = -1;
 	while (++i < 26 && tetramins[i])
 		printf("(%llu): %s\n", tetramins[i], ft_print_bit(tetramins[i]));
 
+	free(tetramins);
 	return (0);
 }
